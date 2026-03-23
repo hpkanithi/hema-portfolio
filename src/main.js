4bf81ef331b8fc353b6401225d9096819fcbec25
Hema Kanithi <hema.wtson@gmail.com>
2026-03-23 08:06:47 -0400

    Initial portfolio setup

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    <section id="intro">
+      <h1>Hema Portfolio</h1>
+      <p>Intro section coming soon</p>
+    </section>
+
+    <section id="experience">
+      <h2>Experience</h2>
+      <p>Experience section coming soon</p>
+    </section>
+
+    <section id="projects">
+      <h2>Projects</h2>
+      <p>Projects section coming soon</p>
+    </section>
+  </main>
+`
